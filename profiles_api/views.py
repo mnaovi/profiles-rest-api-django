e7f3c58b2b7b489deaadce200c34b9fd833d186e
mnaovi <nazmulahsan210@gmail.com>
2021-07-26 10:59:40 +0600

    added demo api response

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test Api View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+            'uses HTTP methods as function(GET, POST, PUT, PATCH, DELETE)',
+            'we are testing view here',
+            'not other thing',
+            'all of the text is for demo purpose'
+        ]
+
+        return Response({'message': 'Hello', 'an_apiview': an_apiview})
